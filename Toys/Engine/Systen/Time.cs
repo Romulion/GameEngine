5c3db9d6fa3c3fbab913569f9c000c980fb846bf
Romulion <Romulion@gmail.com>
2019-09-27 19:59:59 +0300

    added time measuring utilite

---
+++ Toys/Engine/Systen/Time.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Toys
+{
+    public class Time
+    {
+        public double renderTime { get; internal set; }
+        public double updagteTime { get; internal set; }
+        Stopwatch stopwatch;
+        static double resolution;
+
+        internal Time()
+        {
+            stopwatch = new Stopwatch();
+        }
+
+        static Time()
+        {
+            resolution = (double)1000 / Stopwatch.Frequency;
+        }
+
+        public void Start()
+        {
+            stopwatch.Start();
+        }
+
+        public double Stop()
+        {
+            double elapsed;
+            stopwatch.Stop();
+            elapsed = resolution * stopwatch.ElapsedTicks;
+            stopwatch.Reset();
+
+            return elapsed;
+        }
+    }
+}
